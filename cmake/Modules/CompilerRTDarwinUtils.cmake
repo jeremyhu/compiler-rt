ffb35ff4b3e4d07965861613b04df42ff292e454
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2015-11-30 00:52:14 -0500

    MacPorts Only: Don't check for the macosx.internal SDK
    https://llvm.org/bugs/show_bug.cgi?id=25677
    
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- cmake/Modules/CompilerRTDarwinUtils.cmake
+++ cmake/Modules/CompilerRTDarwinUtils.cmake
@@ -4,23 +4,12 @@ include(CMakeParseArguments)
 # set the default Xcode to use. This function finds the SDKs that are present in
 # the current Xcode.
 function(find_darwin_sdk_dir var sdk_name)
-  # Let's first try the internal SDK, otherwise use the public SDK.
-  execute_process(
-    COMMAND xcodebuild -version -sdk ${sdk_name}.internal Path
-    OUTPUT_VARIABLE var_internal
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_FILE /dev/null
-  )
-  if("" STREQUAL "${var_internal}")
     execute_process(
       COMMAND xcodebuild -version -sdk ${sdk_name} Path
       OUTPUT_VARIABLE var_internal
       OUTPUT_STRIP_TRAILING_WHITESPACE
       ERROR_FILE /dev/null
     )
-  else()
-    set(${var}_INTERNAL ${var_internal} PARENT_SCOPE)
-  endif()
   set(${var} ${var_internal} PARENT_SCOPE)
 endfunction()
 
